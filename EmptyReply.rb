c56a3f06d5f541704178a8f6fb7e5edd6fc05888
arc680 <maru.nslab@gmail.com>
2012-06-16 02:34:00 +0900

    first commit

---
+++ EmptyReply.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 空リプ
+
+Module.new do
+  Plugin.create(:EmptyReply).add_event_filter(:command){ |menu|
+    menu[:EmptyReply] = {
+      :slug => :EmptyReply,
+      :name => 'こいつに空リプ',
+      :condition => lambda{ |m| m.message.repliable? },
+      :exec => lambda{ |m|
+        Post.primary_service.update(:message=> "@#{m.message.user.idname}")
+         },
+      :visible => true,
+      :role => :message }
+    [menu]
+  }
+end
